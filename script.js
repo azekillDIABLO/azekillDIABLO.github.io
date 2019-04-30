bc25113514288d6eb6250d1528f8a168b2c59957
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2019-04-30 18:25:05 +0200

    Great "Primavera" Update!
    Fixed basically everything! Patches will come to fix the minor
    disturbances caused by this update as soon as they're found!

--- script.js
+++ script.js
@@ -2,23 +2,19 @@
  YouTube Audio Embed 
  --------------------
  
- Author: Amit Agarwal
+ Author: Amit Agarwal -> Thanks to him :D
  Web: http://www.labnol.org/?p=26740 
- -> Modified by azekill_DIABLO
+ Modifications: azekill_DIABLO
 */
 
-// Deprecated //
-// function onYouTubeIframeAPIReady(){var e=document.getElementById("youtube-audio"),t=document.createElement("img");t.setAttribute("id","youtube-icon"),t.style.cssText="cursor:pointer;cursor:hand",e.appendChild(t);var a=document.createElement("div");a.setAttribute("id","youtube-player"),e.appendChild(a);var o=function(e){var a=e?"https://raw.githubusercontent.com/azekillDIABLO/azekillDIABLO.github.io/master/play.gif":"https://raw.githubusercontent.com/azekillDIABLO/azekillDIABLO.github.io/master/pause.png";t.setAttribute("src",a)};e.onclick=function(){r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))};var r=new YT.Player("youtube-player",{height:"0",width:"0",videoId:e.dataset.video,playerVars:{autoplay:e.dataset.autoplay,loop:e.dataset.loop},events:{onReady:function(e){r.setPlaybackQuality("small"),o(r.getPlayerState()!==YT.PlayerState.CUED)},onStateChange:function(e){e.data===YT.PlayerState.ENDED&&o(!1)}}})}
-
-// function onYouTubeIframeAPIReady(){var e=document.getElementById("youtube-audio"),t=document.createElement("img");t.setAttribute("id","youtube-icon"),t.style.cssText="cursor:pointer;cursor:hand",e.appendChild(t);var a=document.createElement("div");a.setAttribute("id","youtube-player"),e.appendChild(a);var o=function(e){var a=e?"https://raw.githubusercontent.com/azekillDIABLO/azekillDIABLO.github.io/master/play.gif":"https://raw.githubusercontent.com/azekillDIABLO/azekillDIABLO.github.io/master/pause.png";t.setAttribute("src",a)};e.onclick=function(){r.getPlayerState()===YT.PlayerState.PLAYING||r.getPlayerState()===YT.PlayerState.BUFFERING?(r.pauseVideo(),o(!1)):(r.playVideo(),o(!0))};var r=new YT.Player("youtube-player",{height:"0",width:"0",videoId:e.dataset.video,playerVars:{autoplay:e.dataset.autoplay,loop:e.dataset.loop},events:{onReady:function(e){r.setPlaybackQuality("small"),o(r.getPlayerState()!==YT.PlayerState.CUED)},onStateChange:function(e){e.data===YT.PlayerState.ENDED&&o(!1)}}})}
-
 // New version //
-var player;
+
+  var player;
   function onYouTubeIframeAPIReady() {
 
     var ctrlq = document.getElementById("youtube-audio");
     ctrlq.innerHTML = '<img id="youtube-icon" src=""/><div id="youtube-player"></div>';
-    ctrlq.style.cssText = 'auto;cursor:pointer;cursor:hand;display:none';
+    ctrlq.style.cssText = 'width:150px;margin:2em auto;cursor:pointer;cursor:hand;display:none';
     ctrlq.onclick = toggleAudio;
 
     player = new YT.Player('youtube-player', {
@@ -61,4 +57,6 @@ var player;
       togglePlayButton(false); 
     }
   }
+  
+
   
